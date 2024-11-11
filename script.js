fe69cff005e30d8093545bfeca87bdf1a2f5a718
iga waclawska <iga.waclawska@gmail.com>
2024-11-11 08:03:57 +0100

    Give feedback on Assignment 2

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const increaseButton = document.getElementById("increase");
 const decreaseButton = document.getElementById("decrease");
 const resetButton = document.querySelector(".reset");
 
+// Great job creating elements using JavaScript!
+//
+// Here are a few suggestions for further improvement:
+//
+// -  Styles that remain static, such as font size in your case,
+//    can be defined directly in the styles.css file.
+//
+// -  The code that creates the freeShippingNotice and outOfStockNotice
+//    contains some repetition. The repeated parts could be extracted into a function.
 const freeShippingNotice = document.createElement("span");
 freeShippingNotice.textContent = "You have free shipping";
 freeShippingNotice.style.color = "green";
@@ -25,18 +34,19 @@ function updateCounterDisplay() {
   decreaseButton.disabled = count === 0;
   if (count === 10) {
     freeShippingNotice.style.display = "block";
+  } else if (count < 10 || count >= 20) {
+    freeShippingNotice.style.display = "none";
   }
-  else if (count < 10 || count >= 20) {freeShippingNotice.style.display = "none"}
   if (count === countMaxLimit) {
     increaseButton.style.backgroundColor = "red";
     increaseButton.disabled = true;
-    outOfStockNotice.style.display = "block"
+    outOfStockNotice.style.display = "block";
   } else {
     increaseButton.style.backgroundColor = "";
     increaseButton.disabled = false;
+    // Did you mean outOfStockNotice? :)
     outOfStockMessage.style.display = "none";
   }
-
 }
 
 // Event Listeners
@@ -44,15 +54,15 @@ increaseButton.addEventListener("click", () => {
   if (count < countMaxLimit) {
     count++;
     updateCounterDisplay();
- }
+  }
 });
 
+// Good work on fixing the decreaseButton!
 decreaseButton.addEventListener("click", () => {
-  if(count > 0)
-    {
-       count--;
-       updateCounterDisplay();
-    }
+  if (count > 0) {
+    count--;
+    updateCounterDisplay();
+  }
 });
 
 resetButton.addEventListener("click", () => {
